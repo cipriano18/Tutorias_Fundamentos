74729d242052b0862cca59b220bf452bb78138bc
CiprianoRiveraEscobar <ciprianoantonioriveraescobar@gmail.com>
2026-08-04 10:34:57 -0600

    clase 2

---
+++ Clase 02/Clase # 2/Clase # 2/Clase # 2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+/*
+Que es una variable
+si yo quiero guardar la edad 
+el nombre de la persona
+*/
+int edad;//para guardar numeros
+string nombre; // sirve para guardar palabras
+float altura;//sirve para guardar decimales
+double pi;//sirve para guardar 12.0123 -> 12.012323
+bool aprovado; //  true o false 1 y 0
+char letra;//sirve para guardar un caracter
+const int dias = 7;
+const float pi1 = 3.1416;
+/*
+operadores de asignacion 
+edad=edad+2;
+edad+=2;
+edad*=2;
+edad-=2;
+edad/=2;
+*/
+int main() {
+	double nota1 = 0.0, nota2 = 0.0, nota3 = 0.0, promedio = 0.0;
+	/*cout sirve para mostar datos por consola
+	 sintexis cout<<"Hola "<<endl;  */
+	 /*cin sirve para que el usuario me ingrese los datos por consola
+	 ingrese nota =80
+	 sintaxis cin>>nombre de la variable;
+	 */
+	cout << "Ingrese el valor de la nota1" << endl;
+	cin >> nota1;  
+}
+
